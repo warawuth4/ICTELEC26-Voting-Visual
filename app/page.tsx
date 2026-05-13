15529bb3cc8bcf2cf6e4cb637d29dd19d4513382
DRPITOS <123379947+DRPITOS@users.noreply.github.com>
2026-05-13 15:01:06 +0700

    Finalize version

--- app/page.tsx
+++ app/page.tsx
@@ -40,102 +40,102 @@ interface RoundConfig {
 
 // ════════════════════ DATA ════════════════════ //
 const ELECTION_ROUNDS: RoundConfig[] = [
-    // {
-    //     title: "Student Association Party",
-    //     filename: "2026 Student Association Party E-Ballot Voting (All students).csv",
-    //     backgroundImage: "/BackgroundImage/Party.png",
-    //     partyOrder: ["P1", "P2", "PX", "PD"],
-    //     ballotChoices: ["P1", "P2", "PX"],
-    //     partyConfig: {
-    //         P1: {
-    //             name: "Endorse (รับรอง)",
-    //             color: "#2563eb",
-    //             lightColor: "#93bbfd",
-    //             keywords: ["endorse", "รับรอง"]
-    //         },
-    //         P2: {
-    //             name: "Not endorse (ไม่รับรอง)",
-    //             color: "#ea580c",
-    //             lightColor: "#fdba74",
-    //             keywords: ["not endorse", "ไม่รับรอง"]
-    //         },
-    //         PX: {
-    //             name: "No Vote (ไม่ประสงค์ลงคะแนน)",
-    //             color: "#94a3b8",
-    //             lightColor: "#cbd5e1",
-    //             keywords: ["no vote", "ไม่ประสงค์"]
-    //         },
-    //         PD: {
-    //             name: "Invalid Vote (บัตรเสีย)",
-    //             color: "#ef4444",
-    //             lightColor: "#fca5a5",
-    //             keywords: []},
-    //     }
-    // },
-    // {
-    //     title: "Student Representative (ICT Year 1)",
-    //     filename: "2026 Student Representative E-Ballot Voting (ICT ID68).csv",
-    //     backgroundImage: "/BackgroundImage/ICT1.png",
-    //     partyOrder: ["C1", "C2", "PX", "PD"],
-    //     ballotChoices: ["C1", "C2", "PX"],
-    //     partyConfig: {
-    //         C1: {
-    //             name: "[1] 6888041 Aryan Khan",
-    //             color: "#10b981",
-    //             lightColor: "#6ee7b7",
-    //             keywords: ["[1]"]
-    //         },
-    //         C2: {
-    //             name: "[2] 6888188 Monsiri Yutthasarnsenee",
-    //             color: "#8b5cf6",
-    //             lightColor: "#c4b5fd",
-    //             keywords: ["[2]"]
-    //         },
-    //         PX: {
-    //             name: "No Vote (ไม่ประสงค์ลงคะแนน)",
-    //             color: "#94a3b8",
-    //             lightColor: "#cbd5e1",
-    //             keywords: ["no vote", "ไม่ประสงค์"]
-    //         },
-    //         PD: {
-    //             name: "Invalid Vote (บัตรเสีย)",
-    //             color: "#ef4444",
-    //             lightColor: "#fca5a5",
-    //             keywords: []},
-    //     }
-    // },
-    // {
-    //     title: "Student Representative (ICT Year 2)",
-    //     filename: "2026 Student Representative E-Ballot Voting (ICT ID67).csv",
-    //     backgroundImage: "/BackgroundImage/ICT2.png",
-    //     partyOrder: ["P1", "P2", "PX", "PD"],
-    //     ballotChoices: ["P1", "P2", "PX"],
-    //     partyConfig: {
-    //         P1: {
-    //             name: "Endorse (รับรอง)",
-    //             color: "#2563eb",
-    //             lightColor: "#93bbfd",
-    //             keywords: ["endorse", "รับรอง"]
-    //         },
-    //         P2: {
-    //             name: "Not endorse (ไม่รับรอง)",
-    //             color: "#ea580c",
-    //             lightColor: "#fdba74",
-    //             keywords: ["not endorse", "ไม่รับรอง"]
-    //         },
-    //         PX: {
-    //             name: "No Vote (ไม่ประสงค์ลงคะแนน)",
-    //             color: "#94a3b8",
-    //             lightColor: "#cbd5e1",
-    //             keywords: ["no vote", "ไม่ประสงค์"]
-    //         },
-    //         PD: {
-    //             name: "Invalid Vote (บัตรเสีย)",
-    //             color: "#ef4444",
-    //             lightColor: "#fca5a5",
-    //             keywords: []},
-    //     }
-    // },
+    {
+        title: "Student Association Party",
+        filename: "2026 Student Association Party E-Ballot Voting (All students).csv",
+        backgroundImage: "/BackgroundImage/Party.png",
+        partyOrder: ["P1", "P2", "PX", "PD"],
+        ballotChoices: ["P1", "P2", "PX"],
+        partyConfig: {
+            P1: {
+                name: "Endorse (รับรอง)",
+                color: "#2563eb",
+                lightColor: "#93bbfd",
+                keywords: ["endorse", "รับรอง"]
+            },
+            P2: {
+                name: "Not endorse (ไม่รับรอง)",
+                color: "#ea580c",
+                lightColor: "#fdba74",
+                keywords: ["not endorse", "ไม่รับรอง"]
+            },
+            PX: {
+                name: "No Vote (ไม่ประสงค์ลงคะแนน)",
+                color: "#94a3b8",
+                lightColor: "#cbd5e1",
+                keywords: ["no vote", "ไม่ประสงค์"]
+            },
+            PD: {
+                name: "Invalid Vote (บัตรเสีย)",
+                color: "#ef4444",
+                lightColor: "#fca5a5",
+                keywords: []},
+        }
+    },
+    {
+        title: "Student Representative (ICT Year 1)",
+        filename: "2026 Student Representative E-Ballot Voting (ICT ID68).csv",
+        backgroundImage: "/BackgroundImage/ICT1.png",
+        partyOrder: ["C1", "C2", "PX", "PD"],
+        ballotChoices: ["C1", "C2", "PX"],
+        partyConfig: {
+            C1: {
+                name: "[1] 6888041 Aryan Khan",
+                color: "#10b981",
+                lightColor: "#6ee7b7",
+                keywords: ["[1]"]
+            },
+            C2: {
+                name: "[2] 6888188 Monsiri Yutthasarnsenee",
+                color: "#8b5cf6",
+                lightColor: "#c4b5fd",
+                keywords: ["[2]"]
+            },
+            PX: {
+                name: "No Vote (ไม่ประสงค์ลงคะแนน)",
+                color: "#94a3b8",
+                lightColor: "#cbd5e1",
+                keywords: ["no vote", "ไม่ประสงค์"]
+            },
+            PD: {
+                name: "Invalid Vote (บัตรเสีย)",
+                color: "#ef4444",
+                lightColor: "#fca5a5",
+                keywords: []},
+        }
+    },
+    {
+        title: "Student Representative (ICT Year 2)",
+        filename: "2026 Student Representative E-Ballot Voting (ICT ID67).csv",
+        backgroundImage: "/BackgroundImage/ICT2.png",
+        partyOrder: ["P1", "P2", "PX", "PD"],
+        ballotChoices: ["P1", "P2", "PX"],
+        partyConfig: {
+            P1: {
+                name: "Endorse (รับรอง)",
+                color: "#2563eb",
+                lightColor: "#93bbfd",
+                keywords: ["endorse", "รับรอง"]
+            },
+            P2: {
+                name: "Not endorse (ไม่รับรอง)",
+                color: "#ea580c",
+                lightColor: "#fdba74",
+                keywords: ["not endorse", "ไม่รับรอง"]
+            },
+            PX: {
+                name: "No Vote (ไม่ประสงค์ลงคะแนน)",
+                color: "#94a3b8",
+                lightColor: "#cbd5e1",
+                keywords: ["no vote", "ไม่ประสงค์"]
+            },
+            PD: {
+                name: "Invalid Vote (บัตรเสีย)",
+                color: "#ef4444",
+                lightColor: "#fca5a5",
+                keywords: []},
+        }
+    },
     {
         title: "Student Representative (ICT Year 3)",
         filename: "2026 Student Representative E-Ballot Voting (ICT ID66).csv",
